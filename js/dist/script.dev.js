bc4e6f0199fd020565f78d3861902e584c6a620f
Thabata Amanda Gomide <thabataamanda@hotmail.com>
2023-08-09 01:30:35 -0300

    :lipstick: feat|responsive

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -20,27 +20,72 @@ var cloneItems = function cloneItems() {
 
 cloneItems();
 
-function scrollSuave(target) {
+var formatPhone = function formatPhone() {
+  var input = document.getElementById("phoneNumber");
+  var valueNumberPhone = input.value.replace(/\D/g, "");
+
+  if (valueNumberPhone.length > 2) {
+    var ddd = valueNumberPhone.slice(0, 2);
+    var otherNumbers = valueNumberPhone.slice(2, 10); // Limitando para 8 dígitos após o DDD
+
+    valueNumberPhone = ddd + otherNumbers;
+  }
+
+  if (valueNumberPhone.length >= 11) {
+    valueNumberPhone = valueNumberPhone.replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+  } else if (valueNumberPhone.length >= 6) {
+    valueNumberPhone = valueNumberPhone.replace(/^(\d{2})(\d{4})(\d{0,4})/, "($1) $2-$3");
+  } else if (valueNumberPhone.length >= 2) {
+    valueNumberPhone = valueNumberPhone.replace(/^(\d{2})(\d{0,5})/, "($1) $2");
+  }
+
+  input.value = valueNumberPhone;
+};
+
+document.getElementById("phoneNumber").addEventListener("input", formatPhone);
+
+var handleScroll = function handleScroll() {
+  var navigation = document.querySelector("nav");
+  var scrollY = window.scrollY;
+
+  if (scrollY > 0) {
+    navigation.style.position = "fixed";
+    navigation.style.backdropFilter = "blur(10px)";
+    navigation.style.width = "100%";
+    navigation.style.padding = "10px 25px";
+    navigation.style.left = "0";
+    navigation.style.zIndex = "99";
+  } else {
+    navigation.style.position = "";
+    navigation.style.backdropFilter = "";
+    navigation.style.width = "";
+    navigation.style.padding = "";
+    navigation.style.left = "";
+    navigation.style.zIndex = "";
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
+
+function scrollSmooth(target) {
   var element = document.querySelector(target);
   var elementPosition = element.getBoundingClientRect().top;
-  var offsetPosition = elementPosition - 30; // Ajuste o valor do offset conforme necessário
+  var offsetPosition = elementPosition - 30; //
 
   window.scrollBy({
     top: offsetPosition,
     behavior: 'smooth'
   });
-} // Adiciona um evento de clique para os links internos
-
+}
 
 document.querySelectorAll('nav a').forEach(function (link) {
   link.addEventListener('click', function (e) {
     e.preventDefault();
     var href = this.getAttribute('href');
-    scrollSuave(href);
+    scrollSmooth(href);
   });
 });
 document.addEventListener("DOMContentLoaded", function () {
-  var carrosselPlans = document.querySelector(".carrossel-plans");
   var planBoxes = document.querySelectorAll(".plan-box");
   var leftArrow = document.querySelector(".arrow-left");
   var rightArrow = document.querySelector(".arrow-right");
@@ -69,8 +114,19 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     leftArrow.addEventListener("click", goToPreviousPlan);
-    rightArrow.addEventListener("click", goToNextPlan); // Exibindo o primeiro plano como padrão
-
+    rightArrow.addEventListener("click", goToNextPlan);
     showCurrentPlan();
   }
 });
+var phrases = document.querySelectorAll('.phrase');
+var currentIndex = 0;
+
+var showNextPhrase = function showNextPhrase() {
+  phrases[currentIndex].style.display = 'none';
+  currentIndex = (currentIndex + 1) % phrases.length;
+  phrases[currentIndex].style.display = 'block';
+  setTimeout(showNextPhrase, 5000);
+};
+
+phrases[currentIndex].style.display = 'block';
+setTimeout(showNextPhrase, 5000);
--- js/script.js
+++ js/script.js
@@ -20,10 +20,55 @@ const cloneItems = () => {
 cloneItems();
 
 
-function scrollSuave(target) {
+const formatPhone = () => {
+  const input = document.getElementById("phoneNumber");
+  let valueNumberPhone = input.value.replace(/\D/g, "");
+
+  if (valueNumberPhone.length > 2) {
+    const ddd = valueNumberPhone.slice(0, 2);
+    const otherNumbers = valueNumberPhone.slice(2, 10); // Limitando para 8 dígitos após o DDD
+    valueNumberPhone = ddd + otherNumbers;
+  }
+
+  if (valueNumberPhone.length >= 11) {
+    valueNumberPhone = valueNumberPhone.replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+  } else if (valueNumberPhone.length >= 6) {
+    valueNumberPhone = valueNumberPhone.replace(/^(\d{2})(\d{4})(\d{0,4})/, "($1) $2-$3");
+  } else if (valueNumberPhone.length >= 2) {
+    valueNumberPhone = valueNumberPhone.replace(/^(\d{2})(\d{0,5})/, "($1) $2");
+  }
+
+  input.value = valueNumberPhone;
+}
+document.getElementById("phoneNumber").addEventListener("input", formatPhone);
+
+
+const handleScroll = () => {
+  const navigation = document.querySelector("nav");
+  const scrollY = window.scrollY;
+  if (scrollY > 0) {
+    navigation.style.position = "fixed";
+    navigation.style.backdropFilter = "blur(10px)";
+    navigation.style.width = "100%";
+    navigation.style.padding = "10px 25px";
+    navigation.style.left = "0";
+    navigation.style.zIndex = "99";
+  } else {
+    navigation.style.position = "";
+    navigation.style.backdropFilter = "";
+    navigation.style.width = "";
+    navigation.style.padding = "";
+    navigation.style.left = "";
+    navigation.style.zIndex = "";
+  }
+};
+window.addEventListener("scroll", handleScroll);
+
+
+function scrollSmooth(target) {
   const element = document.querySelector(target);
   const elementPosition = element.getBoundingClientRect().top;
-  const offsetPosition = elementPosition - 30; // Ajuste o valor do offset conforme necessário
+  const offsetPosition = elementPosition - 30; //
 
   window.scrollBy({
     top: offsetPosition,
@@ -31,18 +76,17 @@ function scrollSuave(target) {
   });
 }
 
-// Adiciona um evento de clique para os links internos
+
 document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', function (e) {
     e.preventDefault();
     const href = this.getAttribute('href');
-    scrollSuave(href);
+    scrollSmooth(href);
   });
 });
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  const carrosselPlans = document.querySelector(".carrossel-plans");
   const planBoxes = document.querySelectorAll(".plan-box");
   const leftArrow = document.querySelector(".arrow-left");
   const rightArrow = document.querySelector(".arrow-right");
@@ -50,7 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const screenWidth = window.innerWidth;
 
   if (screenWidth === 1024) {
-    function showCurrentPlan() {
+    const showCurrentPlan = () => {
       planBoxes.forEach((box, index) => {
         if (index === currentPlanIndex) {
           box.style.display = "block";
@@ -60,12 +104,13 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
 
-    function goToNextPlan() {
+    const goToNextPlan = () => {
       currentPlanIndex = (currentPlanIndex + 1) % planBoxes.length;
       showCurrentPlan();
     }
 
-    function goToPreviousPlan() {
+
+    const goToPreviousPlan = () => {
       currentPlanIndex = (currentPlanIndex - 1 + planBoxes.length) % planBoxes.length;
       showCurrentPlan();
     }
@@ -73,7 +118,19 @@ document.addEventListener("DOMContentLoaded", function () {
     leftArrow.addEventListener("click", goToPreviousPlan);
     rightArrow.addEventListener("click", goToNextPlan);
 
-    // Exibindo o primeiro plano como padrão
     showCurrentPlan();
   }
 });
+
+const phrases = document.querySelectorAll('.phrase');
+let currentIndex = 0;
+
+const showNextPhrase = () => {
+  phrases[currentIndex].style.display = 'none';
+  currentIndex = (currentIndex + 1) % phrases.length;
+  phrases[currentIndex].style.display = 'block';
+  setTimeout(showNextPhrase, 5000);
+}
+
+phrases[currentIndex].style.display = 'block';
+setTimeout(showNextPhrase, 5000);
